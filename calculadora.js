ef5c7163d816a70e3a89fe3090ad56384fb02b39
Alan Rodrigues <alan-slip20@hotmail.com>
2021-10-28 12:09:53 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function insert(num){
+   var numero = document.getElementById('display').innerHTML;
+   document.getElementById('display').innerHTML = numero + num;
+
+}
+
+function limpar(){
+    document.getElementById('display').innerHTML = '';
+}
+
+
+function back(){
+    var display = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = display.substring(0,display.length -1)
+}
+
+function calcular(){
+    var display = document.getElementById('display').innerHTML;
+    if(display){
+        document.getElementById('display').innerHTML = eval(display);
+    }else{
+        document.getElementById('display').innerHTML = "calculo"
+    }
+}
